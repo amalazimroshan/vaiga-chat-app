d587f6b18cb0abd5eb665dd9ab6300d3a77a873c
aswinr <aswinr019@gmail.com>
2023-02-25 10:29:52 +0530

    Added translation

--- chat_app/routes.py
+++ chat_app/routes.py
@@ -1,11 +1,15 @@
 from chat_app import app
 from flask import render_template , jsonify
-
+from googletrans import Translator
 
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-#@app.routes('/chat')
+@app.route('/chat/<string:message>',methods=['GET','POST'])
+def chat(message):
+    translator = Translator()
+    translated = translator.translate(message,dest='en').text 
+    return jsonify({"message":""+translated})
 
