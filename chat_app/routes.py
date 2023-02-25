af0877f7dcf8a030086c5c7dcb1e00875da642aa
roshan <amalazimroshan@gmail.com>
2023-02-25 11:15:50 +0530

    no change commit

--- chat_app/routes.py
+++ chat_app/routes.py
@@ -1,5 +1,5 @@
 from chat_app import app
-from flask import render_template , jsonify
+from flask import render_template, jsonify
 from googletrans import Translator
 
 
@@ -7,9 +7,9 @@ from googletrans import Translator
 def home_page():
     return render_template('index.html')
 
-@app.route('/chat/<string:message>',methods=['GET','POST'])
+
+@app.route('/chat/<string:message>', methods=['GET', 'POST'])
 def chat(message):
     translator = Translator()
-    translated = translator.translate(message,dest='en').text 
-    return jsonify({"message":""+translated})
-
+    translated = translator.translate(message, dest='en').text
+    return jsonify({"message": ""+translated})
