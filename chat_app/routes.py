00d0634e8d0b8cb6812195215bcea2bf0b1dc499
roshan <amalazimroshan@gmail.com>
2023-02-25 12:08:47 +0530

    working file of getting translated text as msg

--- chat_app/routes.py
+++ chat_app/routes.py
@@ -8,8 +8,8 @@ def home_page():
     return render_template('index.html')
 
 
-@app.route('/chat/<string:message>', methods=['GET', 'POST'])
+@app.route('/<string:message>', methods=['GET', 'POST'])
 def chat(message):
     translator = Translator()
     translated = translator.translate(message, dest='en').text
-    return jsonify({"message": ""+translated})
+    return jsonify({"message": "hello "+translated})
