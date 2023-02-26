935b5ffc8921739ba4e088f5c3d202556afbed7d
roshan <amalazimroshan@gmail.com>
2023-02-26 13:49:50 +0530

    not working file for roshan but aswin speak

--- chat_app/static/speech.js
+++ chat_app/static/speech.js
@@ -1,25 +1,22 @@
 const speechBtn = document.getElementById("start-speech");
-const speakBtn = document.getElementById("speak-btn");
+
 // const lastBubble = document.querySelectorAll(".robot-aligner");
 
-
-
-
 const synth = window.speechSynthesis;
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 recognition.lang = "ml-IN";
 
-
 let query = "";
 recognition.addEventListener("result", (e) => {
   const text = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
-    query = text; 
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+  query = text;
 });
 
 recognition.addEventListener("end", () => {
@@ -31,25 +28,33 @@ speechBtn.addEventListener("click", (e) => {
   e.preventDefault();
   recognition.start();
   console.log(document.querySelectorAll(".robot-aligner"));
-  
 });
 
-
 function speak(text) {
-
   const utterance = new SpeechSynthesisUtterance(text);
 
   // Set the speech settings for Malayalam language
-  utterance.lang = 'ml-IN'; // Malayalam (India) language code
+  utterance.lang = "ml-IN"; // Malayalam (India) language code
   utterance.pitch = 1.0;
   utterance.rate = 1.0;
   utterance.volume = 1.0;
 
   // Set the Malayalam voice
-  const malayalamVoice = synth.getVoices().find(voice => voice.lang === 'ml-IN');
+  const malayalamVoice = synth
+    .getVoices()
+    .find((voice) => voice.lang === "ml-IN");
   if (malayalamVoice) {
     utterance.voice = malayalamVoice;
   }
 
   synth.speak(utterance);
 }
+
+document.body.addEventListener("keyup", (e) => {
+  if (e.key == "o") {
+    // let bubbles = document.querySelectorAll(".bubble");
+    // console.log(bubbles[bubbles.length - 1].textContent);
+    console.log("o");
+    speak("ഹായ്");
+  }
+});
